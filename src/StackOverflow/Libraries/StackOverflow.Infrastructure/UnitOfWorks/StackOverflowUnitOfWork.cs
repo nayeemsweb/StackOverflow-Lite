b9fd14e81705db8c498b564b9f9b80d3019a9367
Nayeem Rahman <nayeem12rahman@gmail.com>
2022-09-04 01:02:45 +0600

    StackOverflowUnitOfWork & IStackOverflowUnitOfWork #1

---
+++ src/StackOverflow/Libraries/StackOverflow.Infrastructure/UnitOfWorks/StackOverflowUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using StackOverflow.Infrastructure.Data;
+using StackOverflow.Infrastructure.DbContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflow.Infrastructure.UnitOfWorks
+{
+    public class StackOverflowUnitOfWork : UnitOfWork, IStackOverflowUnitOfWork
+    {
+        public StackOverflowUnitOfWork(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
