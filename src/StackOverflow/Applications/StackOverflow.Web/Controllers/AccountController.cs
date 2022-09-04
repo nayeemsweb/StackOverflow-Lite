f006036232bf74f01fe5a95f1fd7466cd5e33655
Nayeem Rahman <nayeem12rahman@gmail.com>
2022-09-04 15:39:55 +0600

    Login + Register Page #5

---
+++ src/StackOverflow/Applications/StackOverflow.Web/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StackOverflow.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
