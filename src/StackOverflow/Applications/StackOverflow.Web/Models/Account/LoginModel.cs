4ff083314f1201874898d06c1668cfa7b5943e5d
Nayeem Rahman <nayeem12rahman@gmail.com>
2022-09-04 20:23:50 +0600

    Login & Register model added #7

---
+++ src/StackOverflow/Applications/StackOverflow.Web/Models/Account/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.ComponentModel.DataAnnotations;
+
+namespace StackOverflow.Web.Models.Account
+{
+    public class LoginModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public IList<AuthenticationScheme>? ExternalLogins { get; set; }
+        public string? ReturnUrl { get; set; }
+        public string? ErrorMessage { get; set; }
+    }
+}
---
+++ src/StackOverflow/Applications/StackOverflow.Web/Models/Account/RegisterModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StackOverflow.Web.Models.Account
+{
+    public class RegisterModel
+    {
+        [Required]
+        [Display(Name = "Display Name")]
+        public string DisplayName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [NotMapped]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Password not matched!")]
+        public string ConfirmPassword { get; set; }
+
+        public string? ReturnUrl { get; set; }
+
+        public IList<AuthenticationScheme>? ExternalLogins { get; set; }
+    }
+}
