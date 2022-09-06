7eb6476d939a22717f8015e48716decc0f72b5a5
Nayeem Rahman <nayeem12rahman@gmail.com>
2022-09-06 13:38:33 +0600

    Minor Change #11

--- src/StackOverflow/Applications/StackOverflow.Web/WebModule.cs
+++ src/StackOverflow/Applications/StackOverflow.Web/WebModule.cs
@@ -1,12 +1,19 @@
 ﻿using Autofac;
+using StackOverflow.Web.Areas.MyProfile.Models;
 using StackOverflow.Web.Models.Account;
 
 public class WebModule : Module
 {
     protected override void Load(ContainerBuilder builder)
     {
+        //Account Models
         builder.RegisterType<RegisterModel>().AsSelf();
         builder.RegisterType<LoginModel>().AsSelf();
+
+        //Post Models
+        builder.RegisterType<CreatePostModel>().AsSelf();
+        builder.RegisterType<UpdatePostModel>().AsSelf();
+        builder.RegisterType<ListPostModel>().AsSelf();
 
         base.Load(builder);
     }
